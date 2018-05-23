6c1fe1d188ff7580a7b08f58a5bb893e0368944c
Kelvin Jackson <kechpaja@comcast.net>
2018-05-23 04:39:34 +0300

    Cleaned up list of reversed tags

--- div_vid_html.sh
+++ div_vid_html.sh
@@ -10,101 +10,14 @@ in="$1"
 out="$2"
 
 endtags="1h 2h 3h 4h 5h 6h aa aera aeratxet atem bb bus d dbk daeh daeht debme
-         dnammoc dnegel dt eclitra
-ecruos
-edisa
-edoc
-elbat
-elyts
-emarfi
-emit
-erp
-erugif
-esab
-etic
-etouqkcolb
-fd
-gmi
-gnorts
-hl
-idb
-ii
-il
-kcart
-knil
-kram
-ld
-lebal
-led
-llams
-lmth
-lo
-loc
-lu
-marap
-me
-mrof
-na
-naps
-negyek
-nfd
-noitces
-noitpac
-noitpacgif
-noitpo
-nottub
-nu
-ob
-odb
-oediv
-oidua
-pam
-pmas
-pp
-pr
-puorgh
-puorgloc
-puorgtpo
-pus
-qq
-rap
-rav
-rb
-rbba
-rbw
-redeah
-retem
-retoof
-rh
-rt
-savnac
-sliated
-sni
-ss
-sserdda
-ssergorp
-tbr
-tcejbo
-tceles
-tdd
-tesdleif
-ti
-tooft
-tpircs
-tpircson
-ts
-tsilatad
-tupni
-tuptuo
-unem
-uq
-uu
-van
-vid
-ybur
-ydob
-ydobt
-yrammus"
+         dnammoc dnegel dt eclitra ecruos edisa edoc elbat elyts emarfi emit
+         erp erugif esab etic etouqkcolb fd gmi gnorts hl idb ii il kcart knil
+         kram ld lebal led llams lmth lo loc lu marap me mrof na naps negyek nfd
+         noitces noitpac noitpacgif noitpo nottub nu ob odb oediv oidua pam pmas
+         pr puorgh puorgloc puorgtpo pus rap rav rb rbba rbw redeah retem retoof
+         rh rt savnac sliated sni sserdda ssergorp tbr tcejbo tceles tdd 
+         tesdleif ti tooft tpircs tpircson ts tsilatad tupni tuptuo unem uq van
+         vid ybur ydob ydobt yrammus"
 
 
 
